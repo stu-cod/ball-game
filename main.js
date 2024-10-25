f16a1055f653661c0558282a7ec8cbefe585a6d0
Stav Ohana <stavohana8@gmail.com>
2024-10-25 20:19:06 +0300

    Update onBallClick() to increment the ball

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 'use strict'
 
+var ballSize = 100
+
 function onBallClick() {
-    console.log('you are the best');
+    const elBall = document.querySelector(".ball")
+
+    ballSize += 50
     
+    elBall.style.width = `${ballSize}px`
+    elBall.style.height = `${ballSize}px`
+    elBall.innerText = ballSize
 }
