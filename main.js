1d8b564177b00f6820a38fa3b858eb2cd1065589
stu-cod <stavohana8@gmail.com>
2024-10-26 16:14:06 +0300

    Ball change to random color

--- main.js
+++ main.js
@@ -5,7 +5,11 @@ var ballSize = 100
 function onBallClick() {
     const elBall = document.querySelector(".ball")
     const randIncNum = getRandomInt(20, 60) 
+    const randColor = getRandomColor()
+
+    elBall.style.backgroundColor = randColor 
     ballSize += randIncNum
+
     if(ballSize > 400) ballSize = 100
     elBall.style.width = `${ballSize}px`
     elBall.style.height = `${ballSize}px`
